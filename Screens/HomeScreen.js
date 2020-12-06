f08d5a2f01c5bf011eba36a148e84f599d3bde02
SergioRocha92 <69970438+SergioRocha92@users.noreply.github.com>
2020-12-06 14:39:17 -0300

    Add files via upload

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,104 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {Component} from 'react';
+import { StyleSheet, Text, Image, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+
+
+export  class HomeScreen extends Component {
+
+constructor(props){
+    super(props);
+
+    this.state = {
+        loading: false,
+        pokemon: [],
+        url: 'https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20'
+
+    }
+
+}
+
+componentDidMount(){
+    this.getPokemon();
+}
+    getPokemon = () => {
+
+        this.setState({ loading:true})
+
+        fetch(this.state.url)
+        .then(res => res.json() )
+        .then( res => {
+
+            this.setState({
+                pokemon : res.results,
+                url: res.next,
+                loading: false
+
+            })
+
+        })
+
+    };
+
+
+
+    render(){
+        
+
+
+        return(
+            <View style={{flex: 4, paddingTop:45, paddingLeft:2}}>
+                <Text>Pokemon</Text>
+
+                <Image
+          style={styles.tinyLogo}
+          source={{uri: 'https://davidquevedo.neocities.org/imagenes/PokeLogo2.png'}}
+        />
+
+                
+                
+                <FlatList
+                data={this.state.pokemon}
+                renderItem={
+                    ({item}) => <Text> { item.name } </Text>
+                    
+
+
+
+                
+                }
+                keyExtractor={(item, index) => index.toString()  }
+
+                
+
+
+                />
+               
+            </View>
+        );
+
+
+
+        
+    }
+    
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+  }
+  
+  ); 
+
+  
---
+++ Screens/LoginScreen.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+
+import * as Google from 'expo-google-app-auth'
+
+export default class LoginScreen extends Component{
+
+    // Acuerdense de poner su key de Google Console
+    async _signInWithGoogle(){
+
+        try {
+            const result = await Google.logInAsync({
+                androidClientId: "442898029812-5v251b0rkee37haaqo41q4hblss2nnvf.apps.googleusercontent.com",
+                scopes: ['profile', 'email'],
+            });
+    
+            if (result.type === 'success') {
+                try {
+                    this.props.onLogin();
+                } catch (error){
+                    console.log("Something happened " + error);
+                }
+            } else {
+                return { cancelled: true };
+            }
+
+        } catch (e) {
+            return { error: true };
+        }
+
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <Button
+                    onPress={() => this._signInWithGoogle()}
+                    title="Iniciar sesión con Google"
+                />
+            </View>
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
